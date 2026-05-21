ca3ed08acff4fe7a28dad166e596cb2a747f65cb
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-05-22 00:43:07 +0200

    feat(smod): add mod call spec

--- EvmAsm/Evm64/SMod/Compose/Base.lean
+++ EvmAsm/Evm64/SMod/Compose/Base.lean
@@ -41,6 +41,7 @@ import EvmAsm.Evm64.SMod.Compose.SaveRa
 import EvmAsm.Evm64.SMod.Compose.SignBlockSpecs
 import EvmAsm.Evm64.SMod.Compose.PreserveDividendSign
 import EvmAsm.Evm64.SMod.Compose.AbsBlockSpecs
+import EvmAsm.Evm64.SMod.Compose.ModCall
 
 namespace EvmAsm.Evm64.SMod.Compose
 
